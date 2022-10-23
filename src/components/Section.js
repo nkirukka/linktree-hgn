5fa5b69917e0aa63590ab4a567eeb340b270b403
Nkiruka Ebele <specialebele@gmail.com>
2022-10-23 18:11:09 +0100

    Remove defaultProps and replace id and href with appropriate information provided

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import Button from './Button';
-
+import "./styles/Section.css";
 
 const Section = (props) => {
     return (
-        <div>
-            <h1>Link Tree</h1>
-            <Button href="https://training.zuri.team/" id="btn__zuri">Zuri Training</Button>
-            <Button href={props.href}>{ props.title}</Button>
-            <Button>{ props.title}</Button>
-            <Button>{ props.title}</Button>
-            <Button>{ props.title}</Button>
+        <div className='Wrapper'>
+            <Button href="https://training.zuri.team/" id="btn__zuri">Zuri Team</Button>
+            <Button href="https://books.zuri.team/" id="books">Zuri Books</Button>
+            <Button href="https://books.zuri.team/python-for-beginners?ref_id=nkirukka," id="book__python">Python Books</Button>
+            <Button href="https://background.zuri.team/" id="pitch">Background Check for Coders</Button>
+            <Button href="https://books.zuri.team/design-rules" id="book__design">Design Books</Button>
         </div>
     );
 }
 
-Section.defaultProps = {
-    href: "https://zuri.team.com",
-    title: "Zuri x HGN"
-}
 export default Section;
