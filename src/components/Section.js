314a7848ad0bdf7f3b1b25cc698c602aa4cf5cc8
Nkiruka Ebele <specialebele@gmail.com>
2022-10-23 17:22:39 +0100

    Render more buttons

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,6 +7,10 @@ const Section = (props) => {
         <div>
             <h1>Link Tree</h1>
             <Button href="https://training.zuri.team/" id="btn__zuri">Zuri Training</Button>
+            <Button href={props.href}>{ props.title}</Button>
+            <Button>{ props.title}</Button>
+            <Button>{ props.title}</Button>
+            <Button>{ props.title}</Button>
         </div>
     );
 }
