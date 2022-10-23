5fa5b2dd7e11c7c9dc1f7c41c6f2920d7c15dfaf
Nkiruka Ebele <specialebele@gmail.com>
2022-10-23 19:21:15 +0100

    Add proper indentation

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import React from 'react';
 import "../components/styles/Button.css";
 
 const Button = (props) => {
-  return (
-      <>
-          <a className='Btn' id={props.id} href={props.href}>{ props.children}</a>
-      </>
-  )
+    return (
+        <>
+            <a className='Btn'
+                id={props.id}
+                href={props.href}>
+                {props.children}</a>
+        </>
+    );
 }
 
 export default Button;
