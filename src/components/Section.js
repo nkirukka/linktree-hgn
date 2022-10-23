fe6e9dfabe5f3f93601900cc935a668f408c1b5a
Nkiruka Ebele <specialebele@gmail.com>
2022-10-23 18:24:25 +0100

    Change classname to Btn-wrapper

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,13 +4,15 @@ import "./styles/Section.css";
 
 const Section = (props) => {
     return (
-        <div className='Wrapper'>
-            <Button href="https://training.zuri.team/" id="btn__zuri">Zuri Team</Button>
-            <Button href="https://books.zuri.team/" id="books">Zuri Books</Button>
-            <Button href="https://books.zuri.team/python-for-beginners?ref_id=nkirukka," id="book__python">Python Books</Button>
-            <Button href="https://background.zuri.team/" id="pitch">Background Check for Coders</Button>
-            <Button href="https://books.zuri.team/design-rules" id="book__design">Design Books</Button>
-        </div>
+        <section>
+            <div className='Btn-wrapper'>
+                <Button href="https://training.zuri.team/" id="btn__zuri">Zuri Team</Button>
+                <Button href="https://books.zuri.team/" id="books">Zuri Books</Button>
+                <Button href="https://books.zuri.team/python-for-beginners?ref_id=nkirukka," id="book__python">Python Books</Button>
+                <Button href="https://background.zuri.team/" id="pitch">Background Check for Coders</Button>
+                <Button href="https://books.zuri.team/design-rules" id="book__design">Design Books</Button>
+            </div>
+        </section>
     );
 }
 
