5561392128ed9b4d5d08cfdd938a8332ec50652d
Nkiruka Ebele <specialebele@gmail.com>
2022-10-23 17:17:39 +0100

    Import the button component and include defaultProps

---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './Button';
+
+
+const Section = (props) => {
+    return (
+        <div>
+            <h1>Link Tree</h1>
+            <Button href="https://training.zuri.team/" id="btn__zuri">Zuri Training</Button>
+        </div>
+    );
+}
+
+Section.defaultProps = {
+    href: "https://zuri.team.com",
+    title: "Zuri x HGN"
+}
+export default Section;
