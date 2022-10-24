9425e0d97336663c91d7fe15925c3d3f8dc1c53e
Nkiruka Ebele <specialebele@gmail.com>
2022-10-24 07:39:40 +0100

    Add links to the social icons to make them active and redirect on click

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './styles/Socials.css';
-import slack from './imgs/slack.png';
-import github from './imgs/Icongithub.png';
+import slack from './imgs/slack.avif';
+import github from './imgs/github.png';
+import twitter from './imgs/twitter.png';
 
 const Socials = () => {
   return (
       <div className='Socials'>
-          <img src={slack} />
-          <img src={github} />
+          <a href='https://slack.com/nkirukka' id='slack'> <img className='Icons' src={slack} /> </a>
+          <a href='https://github.com/nkirukka'> <img className='Icons' src={github} /> </a>
+          <a href='https://twitter.com/nkirukka' id='twitter'> <img className='Icons' src={twitter} /> </a>
     </div>
   )
 }
