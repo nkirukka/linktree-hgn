4ff78d1b7b6a434f2b61339f7d1396de2d60e435
Nkiruka Ebele <specialebele@gmail.com>
2022-10-24 08:36:33 +0100

    Import Zuri internship logo for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import zuri from './imgs/zuri.png';
 import i4g from './imgs/I4G.png';
+import './styles/Footer.css';
 
 const Footer = () => {
   return (
-      <footer>
+      <footer className='Footer'>
           <img src={zuri} alt='zuri internship logo' /> 
           <p>HNG Internship 9 Frontend Task</p>
           <img src={ i4g} alt='ingressive for good' />
