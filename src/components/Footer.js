37233060c25ec90beb50c865047c50838cf60c13
Nkiruka Ebele <specialebele@gmail.com>
2022-10-24 11:29:43 +0100

    Refactor with prettier indentation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,16 @@ import './styles/Footer.css';
 const Footer = () => {
   return (
       <footer className='Footer'>
-          <img src={zuri} alt='zuri internship logo' /> 
+      <img
+        src={zuri}
+        alt='zuri internship logo'
+      /> 
           <p>HNG Internship 9 Frontend Task</p>
-          <img src={ i4g} alt='ingressive for good' id='i4g' />
+      <img
+        src={i4g}
+        alt='ingressive for good'
+        id='i4g'
+      />
     </footer>
   )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,10 @@ import "./styles/Profile.css";
 const Profile = (props) => {
   return (
       <div className="Profile">
-          <img src={props.src} id="profile__img" alt="profile pic"/>
+      <img src={props.src}
+        id="profile__img"
+        alt="profile pic"
+      />
           <h1 className="Username">Annette Black</h1>
       </div>
   )
