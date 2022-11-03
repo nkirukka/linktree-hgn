4abe21d79a04914a79b5c48857108b0100878e12
Nkiruka Ebele <specialebele@gmail.com>
2022-11-03 11:53:00 +0100

    feat: import and render footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Linktree from './components/Linktree';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Linktree />
+      <Footer/>
     </div>
   );
 }
