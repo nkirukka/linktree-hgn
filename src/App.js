b208ee95fd8ca0780521cea8e862f7195f7f0f9a
Nkiruka Ebele <specialebele@gmail.com>
2022-11-03 15:04:50 +0100

    feat: import pages and implement routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Linktree from './components/Linktree';
+
 import Footer from './components/Footer';
+import Contact from './pages/Contact';
+import NoMatch from './pages/NoMatch';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Linktree />
-      <Footer/>
+      <Routes>
+        <Route path='/' index element={<Linktree />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NoMatch/>} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
