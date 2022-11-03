98b96239ce13c40d8acf1be0b154155bef78ecce
Nkiruka Ebele <specialebele@gmail.com>
2022-11-03 18:48:43 +0100

    feat: import styles for /Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import '../components/styles/Contact.css';
 
 const Contact = () => {
   return (
     <section className='Contact'>
       {/* ### Header */}
-      <header>
+      <header className='Contact__header'>
         <h2>Contact Me</h2>
         <p>Hi there, contact me to ask me about anything you have in mind.</p>
       </header>
@@ -15,37 +15,37 @@ const Contact = () => {
         <div className='First-last__container'>
           
           {/* ### First Name */}
-          <div className='Fname_control controls'>
+          <div className='Controls'>
             <label htmlFor='first_name'>First name</label>
             <input type='text' id='first_name' name='first_name' placeholder='Enter your first name' />
           </div>
 
           {/* ### Last Name */}
-          <div className='Lname_control controls'>
-            <label htmlFor='last_name'>First name</label>
+          <div className='Controls'>
+            <label htmlFor='last_name'>Last name</label>
             <input type='text' id='last_name' name='last_name' placeholder='Enter your last name' />
           </div>
         </div>
 
         {/* ### Email */}
-        <div className='email_control controls'>
+        <div className='Controls'>
           <label htmlFor='email'>Email</label>
           <input type='email' id='email' name='email' placeholder='yourname@email.com'  />
         </div>
 
         {/* ### Textarea */}
-        <div className='message_control controls'>
+        <div className='Controls'>
           <label htmlFor='message'>Message</label>
-          <textarea id='message' placeholder="Send me a message and I'll reply you as soon as possible..."/>
+          <textarea id='message' rows='5' placeholder="Send me a message and I'll reply you as soon as possible..."/>
         </div>
 
         {/* ### Permission */}
-        <div className='checkbox_control'>
+        <div className='Checkbox_control'>
           <input type='checkbox' id='permission' />
           <label htmlFor='permission'>You agree to providing your data to Nkiruka who may contact you.</label>
         </div>
 
-        <button type='submit'>Send message</button>
+        <button type='submit' id='btn__submit'>Send message</button>
       </form>
     </section>
   );
