7b101345e5d691e6fd940d018cc191204fee91c1
Nkiruka Ebele <specialebele@gmail.com>
2022-11-04 11:38:53 +0100

    feat: add values and onchange props to form inputs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import '../components/styles/Contact.css';
 
 const Contact = () => {
+  const initialVals = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    message: '',
+  }
+  const [input, setInput] = useState(initialVals);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleChecked = (e) => {
+    setIsChecked(!isChecked);
+  }
   return (
     <section className='Contact'>
       {/* ### Header */}
@@ -17,31 +36,31 @@ const Contact = () => {
           {/* ### First Name */}
           <div className='Controls'>
             <label htmlFor='first_name'>First name</label>
-            <input type='text' id='first_name' name='first_name' placeholder='Enter your first name' />
+            <input type='text' onChange={handleChange} id='first_name' name='first_name' value={input.first_name} placeholder='Enter your first name' />
           </div>
 
           {/* ### Last Name */}
           <div className='Controls'>
             <label htmlFor='last_name'>Last name</label>
-            <input type='text' id='last_name' name='last_name' placeholder='Enter your last name' />
+            <input type='text' onChange={handleChange} id='last_name' name='last_name' value={input.last_name} placeholder='Enter your last name' />
           </div>
         </div>
 
         {/* ### Email */}
         <div className='Controls'>
           <label htmlFor='email'>Email</label>
-          <input type='email' id='email' name='email' placeholder='yourname@email.com'  />
+          <input type='email' onChange={handleChange} id='email' name='email' value={input.email} placeholder='yourname@email.com'  />
         </div>
 
         {/* ### Textarea */}
         <div className='Controls'>
           <label htmlFor='message'>Message</label>
-          <textarea id='message' rows='5' placeholder="Send me a message and I'll reply you as soon as possible..."/>
+          <textarea id='message' onChange={handleChange} name='message' value={input.message} rows='5' placeholder="Send me a message and I'll reply you as soon as possible..."/>
         </div>
 
         {/* ### Permission */}
         <div className='Checkbox_control'>
-          <input type='checkbox' id='permission' />
+          <input type='checkbox' checked={isChecked} onChange={handleChecked} name='permission' value='' id='permission' />
           <label htmlFor='permission'>You agree to providing your data to Nkiruka who may contact you.</label>
         </div>
 
