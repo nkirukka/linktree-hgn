7d7d1437c4998e83a10235d1205370eddbc8e920
Nkiruka Ebele <specialebele@gmail.com>
2022-11-05 19:09:31 +0100

    feat: add a no match page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import '../components/styles/NoMatch.css';
+
 
 const NoMatch = () => {
   return (
-    <div>NoMatch</div>
+    <div className='NoMatch'>
+      <div className='Oops_container'>
+      <p className="Oops">Oops!</p>
+      <h3>404 - PAGE NOT FOUND</h3>
+        <p className="message">The page you are looking for might have been removed, had
+          its name changed or is temporarily unavailable.
+        </p>
+        <Link to='/'>BACK TO HOMEPAGE</Link>
+      </div>
+    </div>
   )
 }
 
