0bbba6d9b6054e66609680bc761aa530894ac5ee
Nkiruka Ebele <specialebele@gmail.com>
2022-11-05 16:35:52 +0100

    refactor: change to browser router

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <HashRouter>
+  
+      <BrowserRouter>
       <App />
-    </HashRouter>
-    </BrowserRouter>
+      </BrowserRouter>
+  
   </React.StrictMode>
 );
 
